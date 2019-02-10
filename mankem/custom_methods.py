3f45378eb20e3efdfb120bcce24800bb4842b87b
omnison <gavin@gmail.com>
2019-02-10 16:48:00 +0100

    fixed

--- mankem/custom_methods.py
+++ mankem/custom_methods.py
@@ -2,15 +2,16 @@ import frappe
 from frappe.model.mapper import get_mapped_doc
 from frappe import throw, _
 from frappe.model.naming import make_autoname
+from frappe.utils import flt, getdate, add_months, get_last_day, fmt_money, nowdate
 
 
 @frappe.whitelist()
 @frappe.whitelist()
 def set_autoname(doc, method):
-	company_abbr = frappe.db.get_value("Company",doc.company,"Company")
+	company_abbr = frappe.db.get_value("Company",doc.company,"abbr_series")
 	if company_abbr:
-		if doc.doctype="Stock Entry":
-			yy = doc.posting_date.strftime("%Y")
-			mm = doc.posting_date.strftime("%m")
+		if doc.doctype=="Stock Entry":
+			yy = getdate(doc.posting_date).strftime("%y")
+			mm = getdate(doc.posting_date).strftime("%m")
 			series = company_abbr+"SE"+yy+ mm + ".###";
 			doc.name =make_autoname(series)
